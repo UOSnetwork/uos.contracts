296a7554a0794164f31e3ac515d40a84304d8530
Unknown <petr.kotegov@gmail.com>
2018-09-04 13:19:57 +0300

    Transaction history added

--- uos.activity/uos.activity.cpp
+++ uos.activity/uos.activity.cpp
@@ -3,6 +3,7 @@
 namespace UOS {
     void uos_activity::usertouser(const account_name acc_from, const account_name acc_to, uint8_t interaction_type_id)
     {
+        print("usertouser acc_from = ", name{acc_from}, " acc_to = ", name{acc_to}, " interaction_type_id = ", (int)interaction_type_id);
         eosio_assert( acc_from != acc_to, "cannot transfer to self" );
         require_auth( acc_from );
         eosio_assert( is_account( acc_to ), "to account does not exist");
@@ -11,6 +12,7 @@ namespace UOS {
 
     void uos_activity::makecontent(account_name acc, string content_id, uint8_t content_type_id, string parent_content_id)
     {
+        print("makecontent acc = ", name{acc}, " content_id = ", content_id, " content_type_id = ", (int)content_type_id, " parent_content_id = ", parent_content_id);
         require_auth( acc );
 
     }
@@ -18,6 +20,7 @@ namespace UOS {
 
     void uos_activity::usertocont(account_name acc, string content_id, uint8_t interaction_type_id)
     {
+        print("usertocont acc = ", name{acc}, " content_id = ", content_id, " interaction_type_id = ", (int)interaction_type_id);
         require_auth( acc);
     }
 
@@ -55,10 +58,6 @@ namespace UOS {
             });
         }
 
-//    for( const auto& item :  secondary_index ) {
-//        print(" ID=", item.primary_key(), ", value=", item.value, "\n");
-//    }
-
     }
 
     void uos_activity::eraserate(uint64_t index) {
@@ -78,6 +77,49 @@ namespace UOS {
             for (uint64_t i = 0; i < number; i++) {
                 if (rates.begin() != rates.end()) {
                     rates.erase(rates.begin());
+                } else {
+                    //print("empty");
+                    break;
+                }
+            }
+
+        }
+    }
+
+    void uos_activity::savetran(string blocknum, string transaction_id, string timestamp, string action_name, string data) {
+        require_auth(_self);
+
+        trans tr_index(_self, _self);
+
+        auto secondary_index = tr_index.get_index<N(tran_hash)>();
+        auto itr = secondary_index.lower_bound(tran::get_trid_key(transaction_id));
+
+        if (itr->transaction_id == transaction_id) {
+            //do nothing, transaction is already saved
+        } else {
+            //save transaction info
+
+            tr_index.emplace(_self, [&](auto &t) {
+                t.key = tr_index.available_primary_key();;
+                t.blocknum = blocknum;
+                t.transaction_id = transaction_id;
+                t.timestamp = timestamp;
+                t.action_name = action_name;
+                t.data = data;
+            });
+        }
+    }
+
+    void uos_activity::erasetrans(uint64_t number = 0) {
+        require_auth(_self);
+        trans tr_index(_self, _self);
+        if (number == 0) {
+            for (; tr_index.begin() != tr_index.end();)
+                tr_index.erase(tr_index.begin());
+        } else {
+            for (uint64_t i = 0; i < number; i++) {
+                if (tr_index.begin() != tr_index.end()) {
+                    tr_index.erase(tr_index.begin());
                 } else {
                     //print("empty");
                     break;
--- uos.activity/uos.activity.hpp
+++ uos.activity/uos.activity.hpp
@@ -41,6 +41,12 @@ namespace UOS {
         //@abi action
         void erase(uint64_t number);
 
+        //@abi action
+        void savetran(string blocknum, string transaction_id, string timestamp, string action_name, string data);
+
+        //@abi action
+        void erasetrans(uint64_t number);
+
     private:
 
         ///@abi table rate i64
@@ -64,8 +70,34 @@ namespace UOS {
 
         typedef eosio::multi_index<N(rate), rate, indexed_by<N(
                 name_hash), const_mem_fun<rate, key256, &rate::by_name>>> rateIndex;
+
+        ///@abi table tran i64
+        struct tran {
+            uint64_t key;
+            string blocknum;
+            string transaction_id;
+            string timestamp;
+            string action_name;
+            string data;
+
+            uint64_t primary_key() const { return key; }
+
+            key256 by_trid() const { return  get_trid_key(transaction_id); }
+
+            static key256 get_trid_key(const string &trid) {
+                checksum256 result;
+                sha256((char *) trid.c_str(), strlen(&trid[0]), &result);
+                const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&result);
+                return key256::make_from_word_sequence<uint64_t>(p64[0], p64[1], p64[2], p64[3]);
+            }
+
+            EOSLIB_SERIALIZE(tran, (key)(blocknum)(transaction_id)(timestamp)(action_name)(data))
+        };
+
+        typedef eosio::multi_index<N(tran), tran, indexed_by<N(
+                tran_hash), const_mem_fun<tran, key256, &tran::by_trid>>> trans;
     };
 
-    EOSIO_ABI(uos_activity, (usertouser)(makecontent)(usertocont)(setrate)(eraserate)(erase))
+    EOSIO_ABI(uos_activity, (usertouser)(makecontent)(usertocont)(setrate)(eraserate)(erase)(savetran)(erasetrans))
 
 }
