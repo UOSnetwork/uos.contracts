7fd79dd989e0c4d55a2a97540e4a7f10f3db22cb
Unknown <petr.kotegov@gmail.com>
2018-09-26 16:44:26 +0300

    removed the code related to assets and voting

--- uos.calculator/uos.calculator.cpp
+++ uos.calculator/uos.calculator.cpp
@@ -1,5 +1,4 @@
 
-#include <eosio.token/eosio.token.hpp>
 #include "uos.calculator.hpp"
 
 namespace UOS{
@@ -48,48 +47,6 @@ namespace UOS{
         }
     }
 
-    void uos_calculator::stake(const account_name acc, const eosio::asset value) {
-        require_auth(acc);
-
-        eosio_assert(value.symbol.name()==_state.get().base_asset.symbol.name(),"Asset symbol is incompatible with used in contract");
-
-        voters_table voters(_self,acc);
-        auto itr=voters.find(acc);
-        if(itr == voters.end()){
-            voters.emplace(acc,[&](voter_info& a){
-               a.stake = value;
-               a.owner = acc;
-            });
-        }else{
-            voters.modify(itr,acc,[&](voter_info& a){
-               a.stake+=value;
-            });
-        }
-        eosio_assert(is_account(_state.get().fund_name),(std::string("create uos.stake first ")+ name{_state.get().fund_name}.to_string()).c_str());
-        INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {acc, N(active)},
-                                                      { acc, _state.get().fund_name, value, std::string("stake tokens") } );
-
-    }
-
-    void uos_calculator::refund(const account_name acc) {
-        require_auth(acc);
-        voters_table voters(_self,acc);
-        auto itr=voters.find(acc);
-        eosio_assert(itr != voters.end(), "there is nothing to refund here");
-        //todo check timeout
-
-        eosio_assert(is_account(_state.get().fund_name),"create uos.stake first");
-
-        //todo: take back votes from candidates first of all (unvote)
-
-        INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {_state.get().fund_name, N(active)},
-                                                      { _state.get().fund_name, itr->owner, itr->stake, std::string("unstake tokens") } );
-
-
-        voters.erase(itr);
-
-    }
-
     void uos_calculator::iscalc(const account_name acc) {
         require_auth(acc);
         calcs_table calcs(_self,_self);
@@ -103,102 +60,6 @@ namespace UOS{
         if(itr==calcs.end())
             return false;
         return itr->is_active;
-    }
-
-    asset uos_calculator::get_stake(account_name voter) {
-        voters_table voters(_self,voter);
-        auto itr = voters.find(voter);
-        if(itr==voters.end()){
-            asset ret = _state.get().base_asset;
-            ret.amount = 0;
-            return ret;
-        }
-        return itr->stake;
-
-    }
-
-    void uos_calculator::votecalc(const account_name voter, std::vector<account_name> calcs) {
-        require_auth(voter);
-        voters_table voters(_self,voter);
-        auto itr_voter = voters.find(voter);
-        eosio_assert(itr_voter!=voters.end(),"Voter not found in table. Need to stake first");
-        eosio_assert(itr_voter->stake_voted.amount==0,"unvote all calcs first");
-
-        for(auto item : calcs){
-            eosio_assert(is_account(item),(string("account not found: ") + (name{item}).to_string()).c_str());
-            eosio_assert(check_calc(item),(string("account has not been registered or not active: ") + (name{item}).to_string()).c_str());
-        }
-
-        int64_t vote_for_each = get_stake(voter).amount/(int64_t )calcs.size();
-        eosio_assert(vote_for_each>0, "you can not vote zero tokens for each candidate");
-        calcs_table c_table(_self,_self);
-        for(auto item : calcs){
-            auto itr = c_table.find(item);
-            eosio_assert(itr!=c_table.end(),(string("account has not been registered or not active: ") + (name{item}).to_string()).c_str());
-            c_table.modify(itr,_self,[&](calc_info& info){
-               info.total_votes+= static_cast<uint64_t > (vote_for_each);
-            });
-        }
-        auto votesum = vote_for_each*(int64_t)calcs.size();
-
-        voters.modify(itr_voter,voter,[&](voter_info &vi){
-            vi.stake_voted.symbol=vi.stake.symbol;
-            vi.stake_voted.amount = votesum;
-            for(auto calc : calcs){
-                vi.calcs.push_back(candidate_info{calc,vote_for_each});
-            }
-//            vi.calcs = calcs;
-        });
-    }
-
-    void uos_calculator::unvote(const account_name voter, std::vector<account_name> calcs_to_unvote) {
-        require_auth(voter);
-        voters_table voters(_self,voter);
-        auto itr = voters.find(voter);
-        eosio_assert(itr!=voters.end(),"Voter not found");
-
-        //todo: take out this part of code to other function {
-
-        calcs_table c_table(_self,_self);
-        voters.modify(itr,voter,[&](voter_info& vi){
-            //get votes back
-            std::vector<candidate_info> new_list;
-            for(auto calc: vi.calcs){
-                if(std::find(calcs_to_unvote.begin(),calcs_to_unvote.end(),calc.calc)!=calcs_to_unvote.end()){
-                    auto citr = c_table.find(calc.calc);
-                    eosio_assert(citr!=c_table.end(),"Something wrong with data about calc");
-                    c_table.modify(citr,_self,[&](calc_info &ci){
-                        ci.total_votes-= static_cast<uint64_t >(calc.bid);
-                    });
-                    vi.stake_voted.amount-=calc.bid;
-                    calc.bid=0;
-                }else{
-                    new_list.push_back(calc);
-                }
-            }
-            vi.calcs = new_list;
-        });
-
-        //todo: }
-
-    }
-
-    void uos_calculator::unvoteall(const account_name voter) {
-        require_auth(voter);
-        voters_table voters(_self,voter);
-        auto itr = voters.find(voter);
-        eosio_assert(itr!=voters.end(),"Voter not found");
-        std::vector<account_name> calcs_list;
-        for(auto calc : itr->calcs){
-            calcs_list.push_back(calc.calc);
-        }
-        unvote(voter,calcs_list);
-    }
-
-    void uos_calculator::setasset(const eosio::asset value) {
-        require_auth(_self);
-        eosio_assert((_state.get().base_asset.symbol)!=(value.symbol),"Nothing to change");
-        //todo check if there is someone's stake, then..
     }
 
 }
--- uos.calculator/uos.calculator.hpp
+++ uos.calculator/uos.calculator.hpp
@@ -7,9 +7,7 @@
 #include <eosiolib/crypto.h>
 #include <eosiolib/public_key.hpp>
 #include <eosiolib/types.hpp>
-#include <eosiolib/asset.hpp>
 #include <eosiolib/singleton.hpp>
-#include <eosiolib/core_symbol.hpp>
 #include <string>
 #include <cstdint>
 #include <vector>
@@ -20,13 +18,8 @@ namespace UOS{
 
     class uos_calculator: public contract {
     public:
-        uos_calculator(account_name self) : contract(self),_state(_self,_self){
-                if(!_state.exists()){
-                        contract_state temp;
-                        temp.base_asset = asset();
-                        temp.fund_name = N(uos.stake);
-                        _state.set(temp,_self);
-                }
+        uos_calculator(account_name self) : contract(self) {
+
         }
 
         //@abi action
@@ -42,41 +35,9 @@ namespace UOS{
         //@abi action
         void iscalc(const account_name acc);                    //check if account is allowed to calculate activity index. It will use assert. May be add statistics for calculators?
 
-        //@abi action
-        void stake(const account_name voter, const asset value);  //transfer money to stake account
-
-        //@abi action
-        void refund(const account_name voter);                    //todo: transfer money back from stake account +
-
-        //@abi action
-        void votecalc(const account_name voter, std::vector<account_name> calcs);
-
-        //@abi action
-        void unvote(const account_name voter, std::vector<account_name> calcs);
-
-        //@abi action
-        void unvoteall(const account_name voter);
-
-        //@abi action
-        void setasset(const asset value);                       //todo +-
-
         bool check_calc(const account_name calc);
 
-        asset get_stake(account_name);
-
     private:
-
-//        const CALC_NUM = 8;
-
-        //  calc_info is modified producer_info
-
-
-        //@abi table state i64
-        struct contract_state{
-            asset base_asset;
-            account_name fund_name;
-            //todo
-        };
 
         //@abi table calcs i64
         struct calc_info{
@@ -95,36 +56,10 @@ namespace UOS{
 
         };
 
-        struct candidate_info{
-            account_name calc;
-            int64_t bid;
-        };
-
-        //@abi table voters i64
-        struct voter_info{
-            account_name owner=0;
-            std::vector<candidate_info> calcs;
-            asset stake;
-            asset stake_voted;
-            uint64_t primary_key() const {return owner;}
-
-            EOSLIB_SERIALIZE (voter_info, (owner)(calcs)(stake)(stake_voted))
-        };
-
-
-
-
         typedef multi_index <N(calcs), calc_info> calcs_table;
-
-        typedef multi_index <N(voters), voter_info> voters_table;
-
-        typedef singleton <N(state), contract_state> contract_state_singleton;
-
-
-        contract_state_singleton  _state;
 
     };
 
-    EOSIO_ABI(uos_calculator,(regcalc)(rmcalc)(unregcalc)(iscalc)(stake)(refund)(votecalc)(setasset))
+    EOSIO_ABI(uos_calculator,(regcalc)(rmcalc)(unregcalc)(iscalc))
 
 }
