d954ffc78892921e8e086db7f11629d79ae647e8
CooDi <coodi@list.ru>
2019-05-05 19:11:33 +0300

    added uos.airdrop contract

--- contracts/uos.airdrop/uos.airdrop.cpp
+++ contracts/uos.airdrop/uos.airdrop.cpp
@@ -6,8 +6,8 @@ namespace UOS {
     void uos_airdrop::send(uint64_t external_id,
                                  uint64_t airdrop_id,
                                  uint64_t amount,
-                                 account_name acc_name,
-                                 std::string symbol) {
+                                 name acc_name,
+                                 std::string _symbol) {
         require_auth(_self);
 
 //        print("EXTERNAL ID ", external_id, '\n');
@@ -16,25 +16,25 @@ namespace UOS {
 //        print("ACC NAME ", name{acc_name}, '\n');
 //        print("SYMBOL ", symbol, '\n');
 
-        receipt_table r_table(_self, _self);
+        receipt_table r_table(_self, _self.value);
 
 //        print("CHECKING FOR EXTERNAL_ID\n");
-        auto ei_index = r_table.get_index<N(external_id)>();
+        auto ei_index = r_table.get_index<"external.id"_n>();
         auto ei_itr = ei_index.find(external_id);
         eosio_assert(ei_itr == ei_index.end(), "Already have the receipt with the same external_id");
 
 //        print("CHECKING FOR ACC_NAME+AIRDROP_ID\n");
-        auto an_index = r_table.get_index<N(acc_name)>();
-        for(auto an_itr = an_index.find(acc_name); an_itr != an_index.end() && an_itr->acc_name == acc_name; an_itr++){
+        auto an_index = r_table.get_index<"acc.name"_n>();
+        for(auto an_itr = an_index.find(acc_name.value); an_itr != an_index.end() && an_itr->acc_name == acc_name; an_itr++){
 //            print(an_itr->id, " ", an_itr->external_id, " ", an_itr->airdrop_id, " ", name{an_itr->acc_name}, "\n");
             eosio_assert(an_itr->airdrop_id != airdrop_id, "Already have the receipt with the same acc_name and airdrop_id");
         }
 
 //        print("SENDING THE TOKENS\n");
-        eosio::asset ast(amount, eosio::symbol_type(eosio::string_to_symbol(4,symbol.c_str())));
+        eosio::asset ast(amount, eosio::symbol(_symbol,4));
 //        ast.symbol.print();print("\n");
 //        ast.print();print("\n");
-        INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {_self, N(active)},
+        INLINE_ACTION_SENDER(eosio::token, transfer)( "eosio.token"_n, {_self, "active"_n},
                                                       { _self, acc_name, ast, std::string("airdrop") } );
 
 //        print("ADDING THE RECEIPT\n");
@@ -44,9 +44,9 @@ namespace UOS {
             rec.airdrop_id = airdrop_id;
             rec.amount = amount;
             rec.acc_name = acc_name;
-            rec.symbol = symbol;
+            rec.symbol = _symbol;
         });
     }
 
-    EOSIO_ABI( uos_airdrop, (send))
+    EOSIO_DISPATCH( uos_airdrop, (send))
 }
--- contracts/uos.airdrop/uos.airdrop.hpp
+++ contracts/uos.airdrop/uos.airdrop.hpp
@@ -6,8 +6,9 @@
 #include <eosiolib/print.hpp>
 #include <eosiolib/crypto.h>
 #include <eosiolib/public_key.hpp>
-#include <eosiolib/types.hpp>
+//#include <eosiolib/types.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/symbol.hpp>
 #include <eosiolib/singleton.hpp>
 #include <eosio.token/eosio.token.hpp>
 #include <string>
@@ -19,37 +20,38 @@ namespace UOS {
     using namespace eosio;
     using std::string;
 
-    class uos_airdrop : public eosio::contract {
+    class [[eosio::contract("uos.airdrop")]]uos_airdrop : public eosio::contract {
     public:
-        uos_airdrop(account_name self) : contract(self) {}
+        uos_airdrop(name receiver, name code, datastream<const char*> ds)
+                : contract(receiver, code, ds){}
 
-        /// @abi action
+        [[eosio::action]]
         void send(uint64_t external_id,
                         uint64_t airdrop_id,
                         uint64_t amount,
-                        account_name acc_name,
+                        name acc_name,
                         string symbol);
     private:
 
-        ///@abi table receipt
-        struct rec_entry {
+        struct  [[eosio::table]]
+        rec_entry {
             uint64_t id;
             uint64_t external_id;
             uint64_t airdrop_id;
             uint64_t amount;
-            account_name acc_name;
+            name acc_name;
             string symbol;
 
             uint64_t primary_key() const { return id; }
             uint64_t by_external_id() const { return external_id; }
-            uint64_t by_acc_name() const { return acc_name; }
+            uint64_t by_acc_name() const { return acc_name.value; }
 
             EOSLIB_SERIALIZE(rec_entry, (id)(external_id)(airdrop_id)(amount)(acc_name)(symbol))
         };
 
-        typedef multi_index <N(receipt), rec_entry
-                , indexed_by<N(external_id), const_mem_fun<rec_entry, uint_fast64_t , &rec_entry::by_external_id>>
-                , indexed_by<N(acc_name), const_mem_fun<rec_entry, uint_fast64_t , &rec_entry::by_acc_name>>
+        typedef multi_index <"receipt"_n, rec_entry
+                , indexed_by<"external.id"_n, const_mem_fun<rec_entry, uint_fast64_t , &rec_entry::by_external_id>>
+                , indexed_by<"acc.name"_n, const_mem_fun<rec_entry, uint_fast64_t , &rec_entry::by_acc_name>>
         > receipt_table;
 
     };
