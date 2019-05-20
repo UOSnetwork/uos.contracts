1d7db4d4a0320faa4cdfb54846d92535555b8f7d
Peter Kotegov <petr.kotegov@gmail.com>
2019-05-20 19:13:52 +0300

    contract to register uos accounts in eos mainnet

---
+++ contracts/uos.register/uos.register.cpp
@@ -0,0 +1,18 @@
+#include "uos.register.hpp"
+
+namespace UOS {
+
+    void uos_register::regname(name eos_account, name uos_account) {
+        require_auth(eos_account);
+
+        accregister_table ar_table(_self, eos_account.value);
+        auto itr = ar_table.find(eos_account.value);
+        eosio_assert(itr == ar_table.end(), "account name already registered");
+        ar_table.emplace(eos_account, [&](account_register &item) {
+                item.eos_account = eos_account;
+                item.uos_account = uos_account;
+            });
+    }
+
+    EOSIO_DISPATCH( uos_register, (regname))
+}
---
+++ contracts/uos.register/uos.register.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/crypto.h>
+#include <eosiolib/public_key.hpp>
+#include <eosiolib/asset.hpp>
+#include <string>
+#include <cstdint>
+#include <vector>
+
+namespace UOS {
+    using namespace eosio;
+    using std::string;
+
+    class [[eosio::contract("uos.register")]]uos_register : public eosio::contract {
+    public:
+        uos_register(name receiver, name code, datastream<const char*> ds)
+                : contract(receiver, code, ds){}
+
+        [[eosio::action]]
+        void regname(name eos_account, name uos_account);
+    private:
+
+
+        struct [[eosio::table]]
+        account_register {
+            name eos_account;
+            name uos_account;
+
+            uint64_t primary_key() const { return eos_account.value; }
+
+            EOSLIB_SERIALIZE(account_register, (eos_account)(uos_account))
+        };
+
+        typedef multi_index <"accregister"_n, account_register> accregister_table;
+    };
+}
+
+
