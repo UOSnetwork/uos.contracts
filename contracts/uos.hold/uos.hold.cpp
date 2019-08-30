ffe502699c8b4149569e703145118e97f06fc6e4
Peter Kotegov <petr.kotegov@gmail.com>
2019-08-30 17:19:42 +0300

    withdraw action

--- contracts/uos.hold/uos.hold.cpp
+++ contracts/uos.hold/uos.hold.cpp
@@ -39,7 +39,13 @@ namespace UOS {
         print("SYMBOL CODE ", quantity.symbol.code().to_string(), "\n");
         print("MEMO ", memo, "\n");
 
+        if(from == _self) {
+            print("IGNORE OUTGOING TRANSFER\n");
+            return;
+        }
+
         check(quantity.symbol.code().to_string() == "UOS", "only UOS core token can be accepted");
+        check(is_account(name{memo}), "must be an existing account name in memo");
 
         balance_table bals(_self,_self.value);
         auto acc_name = eosio::name(memo);
@@ -65,20 +71,61 @@ namespace UOS {
     }
 
     void uos_hold::withdraw(name acc_name) {
-           print("WITHDRAWW","\n");
-           print("ACC_NAME ", name{acc_name}, "\n");
+        print("WITHDRAW","\n");
+        print("ACC_NAME ", name{acc_name}, "\n");
+        
+        require_auth(acc_name);
+        
+        auto lim_begin = _limits.get().begin;
+        print("BEGIN ", lim_begin, "\n");
+        auto lim_end = _limits.get().end;
+        print("END ", lim_end, "\n");
+        
+        check(0 < lim_begin && lim_begin < lim_end, "limits are not set properly");
+        
+        balance_table bals(_self,_self.value);
+        auto itr = bals.find(acc_name.value);
+        check(itr != bals.end(), "balance record not found");
 
-           require_auth(acc_name);
+        auto current_time = eosio::current_time_point().time_since_epoch()._count;
+        print("CURRENT TIME ", current_time, "\n");
 
-           auto lim_begin = _limits.get().begin;
-           print("BEGIN ", lim_begin, "\n");
-           auto lim_end = _limits.get().end;
-           print("END ", lim_end, "\n");
-           
-           check(0 < lim_begin && lim_begin < lim_end, "limits are not set properly");
+        check(current_time > lim_begin, "withdrawal period not started yet");
 
-           auto current_time = eosio::current_time_point().time_since_epoch()._count;
-           print("CURRENT TIME ", current_time, "\n");
+        uint64_t withdraw_limit = 0;
+        if(current_time > lim_end) {
+            print("FULL DEPOSIT \n");
+            withdraw_limit = itr->deposit;
+        } else {
+            print("SOME PART OF DEPOSIT \n");
+            withdraw_limit = (uint64_t)((float)itr->deposit
+                                      * (float)(current_time - lim_begin)
+                                      / (float)(lim_end - lim_begin));
+        }
+
+        print("DEPOSIT ", itr->deposit, "\n");
+        print("WITHDRAWAL ", itr->withdrawal, "\n");
+        print("WITHDRAW LIMIT ", withdraw_limit, "\n");
+
+        check(itr->withdrawal < withdraw_limit, "nothing to withdraw");
+
+        uint64_t current_withdrawal = withdraw_limit - itr->withdrawal;
+        print("CURRENT WITHDRAWAL ", current_withdrawal, "\n");
+        
+        //send current_withdrawal tokens to account
+        asset ast(current_withdrawal, symbol("UOS",4));
+        action(
+            permission_level{ _self, name{"active"} },
+            name{"eosio.token"}, name{"transfer"},
+            std::make_tuple(_self, acc_name, ast, string("some memo here"))
+        ).send();
+        
+        //increase withdrawal value
+        bals.modify(itr,_self, [&] (balance_entry &item){
+            item.acc_name=itr->acc_name;
+            item.deposit=itr->deposit;
+            item.withdrawal=itr->withdrawal + current_withdrawal;
+        });        
     }
     
     extern "C" {
